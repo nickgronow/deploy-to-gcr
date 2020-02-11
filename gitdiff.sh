e7338b7485f0f8d3052898f3080f6e0c2bb88ee2
Nick Gronow <nick@wov.io>
2020-02-11 07:48:03 -0500

    Enhance gitdiff

--- gitdiff.sh
+++ gitdiff.sh
@@ -1,30 +1,29 @@
 #!/bin/bash
 
-GIT_PREVIOUS_COMMIT=$(git rev-parse --short "HEAD^")
-GIT_COMMIT=$(git rev-parse --short HEAD)
+PREVIOUS_COMMIT=$(git rev-parse --short HEAD^ 2> /dev/null)
+COMMIT=$(git rev-parse --short HEAD 2> /dev/null)
 
 DIR_PATH=$1
 if [ ! -d "$DIR_PATH" ]; then
     echo "Directory '$DIR_PATH' not exists"
-    exit 1
-fi
-
-if [ -z "$GIT_COMMIT" ]; then
-    echo "No current commit... fail"
-    exit 1
-fi
-
-if [ -z "$GIT_PREVIOUS_COMMIT" ]; then
-    echo "No previous commit, files are changed!"
     exit 0
 fi
 
-# Check is files in given directory changed between commits
-# NOTE: $GIT_PREVIOUS_COMMIT and $GIT_COMMIT provided by Jenkins GIT Plugin
-CHANGED=`git diff --name-only $GIT_PREVIOUS_COMMIT $GIT_COMMIT $DIR_PATH`
+if [ -z "$COMMIT" ]; then
+    echo "No current commit... fail"
+    exit 0
+fi
 
-if [ -z "$CHANGED" ]; then
-    echo false
+if [ -z "$PREVIOUS_COMMIT" ]; then
+    echo "No previous commit, files are changed!"
+    exit 1
+fi
+
+# Check is files in given directory changed between commits
+if [ git diff --name-only HEAD^ HEAD $DIR_PATH ]; then
+    echo "Files are changed"
+    exit 1
 else
-    echo true
+    echo "Files remain unchanged"
+    exit 0
 fi
